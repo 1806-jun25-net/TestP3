d413354dddc4b2f615b868d46126e938bcc9b5f6
SHADOW <angel.luis79@yahoo.com>
2018-08-14 11:57:53 -0400

    Add contructure and method

--- TestProject3.Library/Repo/Repo.cs
+++ TestProject3.Library/Repo/Repo.cs
@@ -7,5 +7,18 @@ namespace TestProject3.Repo
 {
     public class Repo
     {
+        private readonly PizzaPalacedbContext _db;
+
+        public Repo(PizzaPalacedbContext db)
+        {
+            _db = db ?? throw new ArgumentNullException(nameof(db));
+        }
+
+        public IEnumerable<Users> GetUsertable()
+        {
+            List<Users> user = _db.Users.ToList();
+            return user;
+
+        }
     }
 }
